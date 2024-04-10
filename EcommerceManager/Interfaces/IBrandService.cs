71ff8fbe8ec183abfc329c78f3c3c07b52582d49
JuMaxi <jmaximovski@gmail.com>
2024-04-10 18:22:11 +0100

    Creating methods to add, delete, update and search Brands into class BrandService.
    Creating Interface class to BrandService.

---
+++ EcommerceManager/Interfaces/IBrandService.cs
@@ -0,0 +1,12 @@
+﻿using EcommerceManager.Models.DataBase;
+
+namespace EcommerceManager.Interfaces
+{
+    public interface IBrandService
+    {
+        public Task InsertNewBrand(Brand brand);
+        public Task<List<Brand>> GetBrandsFromDataBase();
+        public Task UpdateBrand(Brand brand);
+        public Task DeleteBrand(int id);
+    }
+}
---
+++ EcommerceManager/Services/BrandService.cs
@@ -0,0 +1,46 @@
+﻿using EcommerceManager.Interfaces;
+using EcommerceManager.Models.DataBase;
+
+namespace EcommerceManager.Services
+{
+    public class BrandService : IBrandService
+    {
+        private readonly IBrandDbAccess _brandDbAccess;
+        private readonly IValidateBrand _validateBrand;
+
+        public BrandService(IBrandDbAccess brandDbAccess, IValidateBrand validateBrand)
+        {
+            _brandDbAccess = brandDbAccess;
+            _validateBrand = validateBrand;
+        }
+
+        public async Task InsertNewBrand(Brand brand)
+        {
+            await _validateBrand.Validate(brand);
+
+            await _brandDbAccess.AddNewBrand(brand);
+        }
+
+        public async Task<List<Brand>> GetBrandsFromDataBase()
+        {
+            return await _brandDbAccess.GetListBrandsFromDataBase();
+        }
+
+        public async Task UpdateBrand(Brand brand)
+        {
+            await _validateBrand.Validate(brand);
+
+            Brand toUpdate = await _brandDbAccess.GetBrandFromDataBaseById(brand.Id);
+
+            toUpdate.Name = brand.Name;
+            toUpdate.FoundationYear = brand.FoundationYear;
+
+            await _brandDbAccess.UpdateBrand(toUpdate);
+        }
+
+        public async Task DeleteBrand(int id)
+        {
+            await _brandDbAccess.DeleteBrand(id);
+        }
+    }
+}
