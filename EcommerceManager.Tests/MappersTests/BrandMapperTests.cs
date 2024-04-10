263d833ab8b0e9a42412e1d9a70be9bfdc294636
JuMaxi <jmaximovski@gmail.com>
2024-04-10 18:18:59 +0100

    Creating interface to BrandMapper and adding unit tests for it

---
+++ EcommerceManager.Tests/MappersTests/BrandMapperTests.cs
@@ -0,0 +1,51 @@
+﻿using EcommerceManager.Mappers;
+using EcommerceManager.Models.DataBase;
+using EcommerceManager.Models.Requests;
+using EcommerceManager.Models.Responses;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceManager.Tests.MappersTests
+{
+    public class BrandMapperTests
+    {
+        [Fact]
+        public void When_Converting_BrandRequest_It_Should_Be_Equal_Brand()
+        {
+            BrandRequest brandRequest = new()
+            {
+                Name = "Guess",
+                FoundationYear = 1999
+            };
+
+            BrandMapper brandMapper = new();
+
+            Brand brand = brandMapper.ConvertBrandRequestToBrand(brandRequest);
+
+            brand.Name.Should().Be(brandRequest.Name);
+            brand.FoundationYear.Should().Be(brandRequest.FoundationYear);
+        }
+
+        [Fact]
+        public void When_Converting_Brand_It_Should_Be_Equal_BrandResponse()
+        {
+            Brand brand = new()
+            {
+                Name = "Forum",
+                FoundationYear = 1950
+            };
+            List<Brand> listBrand = new() { brand };
+
+            BrandMapper brandMapper = new();
+
+            List<BrandResponse> brandResponse = brandMapper.ConvertBrandToBrandResponse(listBrand);
+
+            brandResponse[0].Name.Should().Be(listBrand[0].Name);
+            brandResponse[0].FoundationYear.Should().Be(listBrand[0].FoundationYear);
+        }
+    }
+}
---
+++ EcommerceManager/Interfaces/IBrandMapper.cs
@@ -0,0 +1,12 @@
+﻿using EcommerceManager.Models.DataBase;
+using EcommerceManager.Models.Requests;
+using EcommerceManager.Models.Responses;
+
+namespace EcommerceManager.Interfaces
+{
+    public interface IBrandMapper
+    {
+        public Brand ConvertBrandRequestToBrand(BrandRequest brandRequest);
+        public List<BrandResponse> ConvertBrandToBrandResponse(List<Brand> brands);
+    }
+}
