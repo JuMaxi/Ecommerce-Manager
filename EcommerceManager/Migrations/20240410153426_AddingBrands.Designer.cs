8a864220d0587d4a3c63df278afd828dded5e4e9
JuMaxi <jmaximovski@gmail.com>
2024-04-10 18:16:42 +0100

    Adding migration to create table Brands into database

--- EcommerceManager/Migrations/20240410153426_AddingBrands.Designer.cs
+++ EcommerceManager/Migrations/20240410153426_AddingBrands.Designer.cs
@@ -4,6 +4,7 @@ using EcommerceManager.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EcommerceManager.Migrations
 {
     [DbContext(typeof(EcommerceManagerDbContext))]
-    partial class EcommerceManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240410153426_AddingBrands")]
+    partial class AddingBrands
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
