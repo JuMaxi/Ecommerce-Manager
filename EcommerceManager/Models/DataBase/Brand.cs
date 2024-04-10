33dae19b7c35398ce9e821cbd85ede6e3348322f
JuMaxi <jmaximovski@gmail.com>
2024-04-10 18:08:35 +0100

    Creating model class to Brand

---
+++ EcommerceManager/Models/DataBase/Brand.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualBasic;
+
+namespace EcommerceManager.Models.DataBase
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int FoundationYear { get; set; }
+    }
+}
