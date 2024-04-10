da1cf1723957b0ff9eb2c0f9f1339a2309977344
JuMaxi <jmaximovski@gmail.com>
2024-04-10 18:28:29 +0100

    Creating end points Post, Get, Put and Delete to Brand

---
+++ EcommerceManager/Controllers/BrandController.cs
@@ -0,0 +1,49 @@
+﻿using EcommerceManager.Interfaces;
+using EcommerceManager.Models.DataBase;
+using EcommerceManager.Models.Requests;
+using EcommerceManager.Models.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceManager.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class BrandController
+    {
+        readonly IBrandService _brandService;
+        readonly IBrandMapper _brandMapper;
+
+        public BrandController(IBrandService brandService, IBrandMapper brandMapper)
+        {
+            _brandService = brandService;
+            _brandMapper = brandMapper;
+        }
+
+        [HttpPost]
+        public async Task AddNewBrand(BrandRequest brandRequest)
+        {
+            await _brandService.InsertNewBrand(_brandMapper.ConvertBrandRequestToBrand(brandRequest));
+        }
+
+        [HttpGet]
+        public async Task<List<BrandResponse>> GetBrandsFromDataBase()
+        {
+            return _brandMapper.ConvertBrandToBrandResponse(await _brandService.GetBrandsFromDataBase());
+        }
+
+        [HttpPut("{id}")]
+        public async Task UpdateBrand([FromRoute] int id, [FromBody] BrandRequest brandRequest)
+        {
+            Brand brand = _brandMapper.ConvertBrandRequestToBrand(brandRequest);
+            brand.Id = id; 
+
+            await _brandService.UpdateBrand(brand);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task DeleteCategory([FromRoute] int id)
+        {
+            await _brandService.DeleteBrand(id);
+        }
+    }
+}
