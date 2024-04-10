a86fd8700f6af6f71397075085f3a9104cb0f5e1
JuMaxi <jmaximovski@gmail.com>
2024-04-10 18:26:38 +0100

    Creating class BrandDbAccess working with EntityFramework.
    Creating interface class to it.

---
+++ EcommerceManager/DbAccess/BrandDbAccess.cs
@@ -0,0 +1,55 @@
+﻿using EcommerceManager.Db;
+using EcommerceManager.Interfaces;
+using EcommerceManager.Models.DataBase;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceManager.DbAccess
+{
+    public class BrandDbAccess : IBrandDbAccess
+    {
+        private readonly EcommerceManagerDbContext _dbContext;
+
+        public BrandDbAccess(EcommerceManagerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddNewBrand(Brand brand)
+        {
+            await _dbContext.AddAsync(brand);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Brand> GetBrandFromDataBaseById(int id)
+        {
+            return await _dbContext.Brands.Where(b => b.Id.Equals(id)).FirstOrDefaultAsync();
+        }
+
+        public async Task<Brand> GetBrandFromDataBaseByName(string name)
+        {
+            return await _dbContext.Brands.Where(b => b.Name == name).FirstOrDefaultAsync();
+        }
+
+        public async Task<Brand> GetBrandFromDataBaseByYearOfFoundation(int year)
+        {
+            return await _dbContext.Brands.Where(b => b.FoundationYear == year).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Brand>> GetListBrandsFromDataBase()
+        {
+            return await _dbContext.Brands.ToListAsync();
+        }
+
+        public async Task UpdateBrand(Brand brand)
+        {
+            _dbContext.Brands.Update(brand);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteBrand(int id)
+        {
+            _dbContext.Brands.Remove(await GetBrandFromDataBaseById(id));
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ EcommerceManager/Interfaces/IBrandDbAccess.cs
@@ -0,0 +1,17 @@
+﻿using EcommerceManager.Models.DataBase;
+
+namespace EcommerceManager.Interfaces
+{
+    public interface IBrandDbAccess
+    {
+        public Task AddNewBrand(Brand brand);
+        public Task<Brand> GetBrandFromDataBaseById(int id);
+
+        public Task<Brand> GetBrandFromDataBaseByName(string name);
+        public Task<Brand> GetBrandFromDataBaseByYearOfFoundation(int year);
+
+        public Task<List<Brand>> GetListBrandsFromDataBase();
+        public Task UpdateBrand(Brand brand);
+        public Task DeleteBrand(int id);
+    }
+}
