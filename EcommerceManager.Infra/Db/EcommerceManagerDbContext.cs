2e37ff1af29461bfd747bdcfcf21eb693d0e3d54
JuMaxi <jmaximovski@gmail.com>
2024-04-15 19:23:19 +0100

    Entity Framework model configuration and adding migration.

--- EcommerceManager.Infra/Db/EcommerceManagerDbContext.cs
+++ EcommerceManager.Infra/Db/EcommerceManagerDbContext.cs
@@ -11,5 +11,23 @@ namespace EcommerceManager.Db
         public DbSet<Category> Categories { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().OwnsOne(p => p.Dimensions, r => 
+            {
+                r.Property(o => o.Width).HasColumnName("width");
+                r.Property(o => o.Height).HasColumnName("height");
+                r.Property(o => o.Length).HasColumnName("length");
+
+            });
+
+            modelBuilder.Entity<Product>().Property(p => p.Name).HasMaxLength(100);
+            modelBuilder.Entity<Product>().Property(p => p.Image).HasMaxLength(400);
+            modelBuilder.Entity<Product>().Property(p => p.Colour).HasMaxLength(20);
+            modelBuilder.Entity<Product>().Property(p => p.SKU).HasMaxLength(20);
+            
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
